1f43cf09dfc5fc185bd62505f684e3d97f3259cc
Josh Woodcock <josh@j0w0.com>
2022-03-26 13:13:34 -0500

    redirect 404 to site index

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,21 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import React, { useEffect } from "react"
+import { Link, navigate } from "gatsby"
 import Layout from "../components/Layout/Layout"
 import SiteMeta from "../components/SiteMeta/SiteMeta"
 
-const NotFoundPage = () => (
-  <Layout>
-    <SiteMeta title="Not Found" />
-    <h1>404</h1>
-    <p>Whoops, couldn't find that page. <Link to="/">Go home.</Link></p>
-  </Layout>
-)
+const NotFoundPage = () => {
+  useEffect(() => {
+    // redirect to index/home
+    navigate('/')
+  }, [])
+  
+  return (
+    <Layout>
+      <SiteMeta title="Not Found" />
+      <h1>404</h1>
+      <p>Whoops, couldn't find that page. <Link to="/">Go home.</Link></p>
+    </Layout>
+  )
+}
 
 export default NotFoundPage
